e6a9cf7af017e700569811ed05ec03a44433f633
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-10-25 20:50:10 -0300

    Adding jakarta persistence

--- src/main/java/paulo/spring/api/controller/ClientController.java
+++ src/main/java/paulo/spring/api/controller/ClientController.java
@@ -4,26 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import paulo.spring.domain.model.Client;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClientController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clients")
     public List<Client> list() {
-        Client client1 = new Client();
-        client1.setId(1L);
-        client1.setName("Name111");
-        client1.setEmail("email@email.com");
-        client1.setNumber("41 98769876");
-
-        Client client2 = new Client();
-        client2.setId(1L);
-        client2.setName("Maria");
-        client2.setEmail("maria@email.com");
-        client2.setNumber("41 67678989");
-
-        return Arrays.asList(client1, client2);
+        return manager.createQuery("from Client", Client.class).getResultList();
     }
 }
--- src/main/java/paulo/spring/domain/model/Client.java
+++ src/main/java/paulo/spring/domain/model/Client.java
@@ -1,15 +1,28 @@
 package paulo.spring.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "client")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(name = "name")
     private String name;
     private String email;
+
+    @Column(name = "phone")
     private String number;
 
 }
